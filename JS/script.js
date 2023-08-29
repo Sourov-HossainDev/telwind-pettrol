c97eff1e6268cc4d9653586a5a2b52d9e9e68f6e
sourov420developer <sourovhossain2326@gmail.com>
2023-08-29 10:24:47 -0700

    done project

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#F40404',
+        },
+        maxWidth:{
+          continer:'1144px',
+
+        },
+        fontFamily: {
+          'pops': ['Poppins', 'sans-serif'],
+          
+        },
+        backgroundImage: {
+          'baner': "url('./img/baner.png')",
+          'p1': "url('./img/p1.png')",
+          'p2': "url('./img/p-2.png')",
+          'p3': "url('./img/p-3.png')",
+          'p4': "url('./img/p-4.png')",
+          's1': "url('./img/s1.png')",
+          's2': "url('./img/s2.png')",
+          's3': "url('./img/s3.png')",
+          'about': "url('./img/about-c.png')",
+          'red1': "url('./img/redmore1.png')",
+          'red2': "url('./img/redmore2.png')",
+          'red3': "url('./img/redmore3.png')",
+          'f1': "url('./img/foter1.png')",
+
+         
+        }
+      }
+    }
+  }
